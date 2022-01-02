ae77db1accfe6a90db409d9d70ecf3648d029fcd
Marlon Beijer <marlon@eevul.org>
2022-01-02 22:52:03 +0100

    Fix build of AmigaOS4 binary

--- platforms/ppc-amigaos/prep.sh
+++ platforms/ppc-amigaos/prep.sh
@@ -7,7 +7,7 @@ set -e
 export TARGET='ppc-amigaos'
 export SYSROOT=/opt/${TARGET}/usr
 export M68K_CPU="-mhard-float"
-export M68K_COMMON="-s -ffast-math -fomit-frame-pointer -noixemul"
+export M68K_COMMON="-s -ffast-math -fomit-frame-pointer -I${SYSROOT}/include -L${SYSROOT}/lib"
 export M68K_CFLAGS="${CFLAGS} ${M68K_CPU} ${M68K_COMMON}"
 export M68K_CXXFLAGS="${CXXFLAGS} ${M68K_CPU} ${M68K_COMMON}"
 export CURPATH="${PWD}"
@@ -17,9 +17,22 @@ export SUBMODULES="${CURPATH}/src/submodules"
 rm -rf "${SUBMODULES}"/zlib/build
 mkdir -p "${SUBMODULES}"/zlib/build
 cd "${SUBMODULES}"/zlib/build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DM68K_COMMON="${M68K_COMMON} -O3 -fno-exceptions -w -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DTOOLCHAIN_COMMON="${M68K_COMMON} -O3 -fno-exceptions -w -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
 cmake --build . --config Release --target install -- -j$(getconf _NPROCESSORS_ONLN)
 cd "${SUBMODULES}"
+
+#MiniGL
+rm -rf MiniGL
+mkdir -p "${SUBMODULES}"/MiniGL
+cd "${SUBMODULES}"/MiniGL
+wget http://os4depot.net/share/driver/graphics/minigl.lha -O minigl.lha
+lha -x minigl.lha
+mkdir -p ${SYSROOT}/usr/include
+mkdir -p ${SYSROOT}/usr/lib
+cp -fvr MiniGL/SDK/local/common/include/* ${SYSROOT}/include/
+cp -fvr MiniGL/SDK/local/newlib/lib/* ${SYSROOT}/lib/
+cd "${SUBMODULES}"
+
 
 # SDL1.2
 if [ ! -d "${SUBMODULES}/SDL" ]; then
@@ -31,7 +44,7 @@ git pull
 rm -rf "${SUBMODULES}"/SDL/build
 mkdir -p "${SUBMODULES}"/SDL/build
 cd "${SUBMODULES}"/SDL/build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DM68K_COMMON="${M68K_COMMON} -O3 -fno-exceptions -w -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DTOOLCHAIN_COMMON="${M68K_COMMON} -O3 -fno-exceptions -w -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
 cmake --build . --config Release --target install -- -j$(getconf _NPROCESSORS_ONLN)
 cd "${SUBMODULES}"
 
