855a3318e9584d965c1155813c3459b9a99401c9
Marlon Beijer <marlon@eevul.org>
2022-01-02 22:00:58 +0100

    Update prep-file for MorphOS

--- platforms/ppc-morphos/prep.sh
+++ platforms/ppc-morphos/prep.sh
@@ -14,12 +14,12 @@ export CURPATH="${PWD}"
 export SUBMODULES="${CURPATH}/src/submodules"
 
 # ZLIB
-rm -rf ${SUBMODULES}/zlib/build
-mkdir -p ${SUBMODULES}/zlib/build
-cd ${SUBMODULES}/zlib/build
+rm -rf "${SUBMODULES}"/zlib/build
+mkdir -p "${SUBMODULES}"/zlib/build
+cd "${SUBMODULES}"/zlib/build
 cmake .. -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DPPC_COMMON="${PPC_COMMON} -O3 -fno-exceptions -w -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
 cmake --build . --config Release --target install -- -j$(getconf _NPROCESSORS_ONLN)
-cd ${SUBMODULES}
+cd "${SUBMODULES}"
 
 # SDL1.2
 rm -rf powersdl_sdk*
@@ -27,14 +27,14 @@ wget http://aminet.net/dev/misc/powersdl_sdk.lha -O powersdl_sdk.lha
 lha -x powersdl_sdk.lha
 mkdir -p ${SYSROOT}/usr
 cp -fvr powersdl_sdk/Developer/usr/local/* ${SYSROOT}/usr/
-cd ${SUBMODULES}
+cd "${SUBMODULES}"
 
 # lhasa
-cd ${SUBMODULES}/lhasa
+cd "${SUBMODULES}"/lhasa
 ./autogen.sh --host=${TARGET}
 CFLAGS="${PPC_CFLAGS}" CXXFLAGS="${PPC_CXXFLAGS}" ./configure --disable-sdltest --disable-shared --enable-static --host=${TARGET} --prefix=${SYSROOT}
 make -j$(getconf _NPROCESSORS_ONLN)
 make install
-cd ${SUBMODULES}
+cd "${SUBMODULES}"
 
-cd ${CURPATH}
+cd "${CURPATH}"
