9cac6e01d4c0496015d13f435000c32a1273bd58
neoman <neoman@titandemo.org>
2022-08-16 12:21:33 +0200

    Cosmetic stuff, restoring old pack behavior

--- build-m68k/src/tracker/version.h
+++
@@ -1,5 +0,0 @@
-// 0xXYYZZ = X.YY.ZZ
-const int MILKYTRACKER_VERSION = 0x2010005;
-// Version string restricted to 20 chars for XM export!
-const char MILKYTRACKER_VERSION_STRING[] = "MilkyTracker 20.10.005";
-//                                          012345678901234567890
